91f84571cae55907fecb7dabc6f40a65dac70385
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-09 14:53:39 -0400

    Update findcp.py

--- findcp.py
+++ findcp.py
@@ -1,13 +1,11 @@
 def findcp():
+    
     print ('This program is to find the cross product of 2 vectors in the form of (a,b,c). ')
     v = eval(input('Please input the first vector. '))
     u = eval(input('Please input the second vector. '))
 
     cp = []
-    cp.append(v[1]*u[2]-v[2]*u[1])
-    cp.append(-(v[0]*u[2]-v[2]*u[0]))
-    cp.append(v[0]*u[1]-v[1]*u[0])
+    cp.extend( (v[1]*u[2]-v[2]*u[1],-v[0]*u[2]+v[2]*u[0],v[0]*u[1]-v[1]*u[0]) )
     print (tuple(cp))
 
 findcp ()
-
